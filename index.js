95788d9fdb148d92c7747bebfdcea6964dfd02ee
benji-devw <bennav.devweb@gmail.com>
2023-02-23 17:57:40 +0100

    update index mongoose.connect

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require ("dotenv").config({path: "./config/config.env"});
 const express = require('express');
   app = express(),
   mongoose = require('mongoose'),
@@ -5,7 +7,9 @@ const express = require('express');
   userRouter = require('./routes/user.router'),
   cors = require('cors');
 
-// console.log(app);
+
+
+  
 //Middleware
 app.use(express.json())
 app.use(function(req, res, next) {
@@ -14,14 +18,18 @@ app.use(function(req, res, next) {
   next()
 });
 
+
+
 // MongoDB
-mongoose.connect('mongodb://127.0.0.1/stouflydoc', { 
-  useNewUrlParser: true, 
-  useUnifiedTopology: true 
-});
+// mongoose.connect('mongodb://127.0.0.1/stouflydoc', { 
+//   useNewUrlParser: true, 
+//   useUnifiedTopology: true 
+// });
+mongoose.connect(process.env.MONGODB_URL, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 })
+  .then (() => console.log("connection to db established..."))
+  .catch(() => console.log("connection to db failed..."))
 
 const db = mongoose.connection;
-
 db.on('error', console.error.bind(console, 'Erreur de connexion : '));
 db.once('open', function() {
   console.log("Connexion à la base de données établie avec succès !");
@@ -33,12 +41,12 @@ app.use(cors());
 
 // ROUTE
 app.use('/tracks', trackRouter);
-app.use('/user', userRouter);
+// app.use('/user', userRouter);
 
 app.use('/public', express.static('public'));
 // app.use('/public', express.static(__dirname + 'public'));
 
-
-app.listen(8080, () => {
-  console.log('Server Ok');
+const PORT = process.env.PORT || 8080
+app.listen(PORT, () => {
+  console.log(`Server listening on port : ${PORT}`);
 })
