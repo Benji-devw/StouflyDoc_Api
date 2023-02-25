508bd2563e2d0ae11789fe886ca8d581b56caebb
benji-devw <bennav.devweb@gmail.com>
2023-02-25 21:13:26 +0100

    update app.use express.static

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ const express = require('express');
   trackRouter = require('./routes/track.router');
   // userRouter = require('./routes/user.router'),
 const cors = require('cors');
+const path = require('path')
 
 
-  
 //Middleware
 app.use(express.json())
 app.use(function(req, res, next) {
@@ -20,13 +20,11 @@ app.use(function(req, res, next) {
 
 
 // MongoDB
-// mongoose.connect('http://localhost:8080/stouflydoc', { 
-//   useNewUrlParser: true, 
-//   useUnifiedTopology: true 
-// });
-mongoose.connect(process.env.DB_URI, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 })
-  .then (() => console.log("connection to db established..."))
-  .catch(() => console.log("connection to db failed..."))
+mongoose.set('strictQuery', true);
+mongoose.connect('mongodb://0.0.0.0:27017/stouflydoc', {useNewUrlParser: true, useUnifiedTopology: true});
+// mongoose.connect(process.env.DB_URI, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 })
+//   .then (() => console.log("connection to db established..."))
+//   .catch(() => console.log("connection to db failed..."))
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'Erreur de connexion : '));
@@ -42,8 +40,8 @@ app.use(cors());
 app.use('/tracks', trackRouter);
 // app.use('/user', userRouter);
 
-app.use('/public', express.static('public'));
-// app.use('/public', express.static(__dirname + 'public'));
+// app.use('/public', express.static('public'));
+app.use('/public', express.static(path.join(__dirname, 'public')))
 
 const PORT = process.env.PORT || 8080
 app.listen(PORT, () => {
